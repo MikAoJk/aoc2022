4071fe8cf4958749d1e38c556f570464ea89eeeb
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2022-12-02 14:50:50 +0100

    init for day 2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,13 @@
 import io.github.mikaojk.day1.part2.day1Part2
 import io.github.mikaojk.day1.part1.day1Part1
+import io.github.mikaojk.day2.day2
 
 fun main() {
     val day1Part1Answer = day1Part1()
     val day1Part2Answer = day1Part2()
+    val day2Answer = day2()
 
     println("Day 1 part 1 answer is: $day1Part1Answer")
     println("Day 1 part 2 answer is: $day1Part2Answer")
+    println("Day 2 answer is: $day2Answer")
 }
